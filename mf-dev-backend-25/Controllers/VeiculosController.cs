32979b01c639738b3dc27800123aa2f8629d78e5
Luiz Felipe Liquer <liquer84@gmail.com>
2025-10-10 23:21:43 -0300

    Editar Veículo

---
+++ mf-dev-backend-25/Controllers/VeiculosController.cs
@@ -0,0 +1,70 @@
+﻿using mf_dev_backend_25.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_25.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        private object await_context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Veiculo veiculo)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Veiculos.Add(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            
+            
+            return View(veiculo);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+                return NotFound();
+            
+            var dados = await _context.Veiculos.FindAsync(id);
+            if(dados == null)
+                return NotFound();  
+
+            return View(dados);
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Edit (int id, Veiculo veiculo)
+        {
+            if(id != veiculo.Id)
+                return NotFound();
+
+            if(ModelState.IsValid)
+            {
+                _context.Veiculos.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
--- mf-dev-backend-25/Models/Veiculo.cs
+++ mf-dev-backend-25/Models/Veiculo.cs
@@ -16,9 +16,12 @@ namespace mf_dev_backend_25.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o ano de fabricação")]
+        [Display(Name = "Ano de fabricação")]
         public int AnoFabricacao { get; set; }
+       
 
         [Required(ErrorMessage = "Obrigatório informar o ano do modelo")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
 
     }
